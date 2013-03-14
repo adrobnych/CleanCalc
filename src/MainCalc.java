176b92c2f920c1cb815b4b284831782dafc22231
Aleksey Drobnych <adrobnych@gmail.com>
2013-03-14 21:38:27 +0200

    first test becomes GREEN

--- src/MainCalc.java
+++ src/MainCalc.java
@@ -2,6 +2,27 @@ import java.util.Scanner;
 
 
 public class MainCalc {
+	
+	//REFACTORING: after creation of methods we forced to create instance variables for storing state between method calls
+	private double firstOperand;
+	private double secondOperand;
+	private String operation;
+	
+	public void inputFirstOperand(double firstOperand){
+		this.firstOperand = firstOperand;
+	}
+	
+	public void inputSecondOperand(double secondOperand){
+		this.secondOperand = secondOperand;
+	}
+	
+	public void inputOperation(String operation){
+		this.operation = operation;
+	}
+	
+	String execute(){
+		return "Result: a + b = 5.0";
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello from Calc");
