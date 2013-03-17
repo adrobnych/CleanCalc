c47258b927c1e112f2ea6bc22d825666c1f87c4d
Aleksey Drobnych <adrobnych@gmail.com>
2013-03-17 20:43:19 +0200

    more failing tests

--- src/CalculatorTestCase.java
+++ src/CalculatorTestCase.java
@@ -16,5 +16,47 @@ public class CalculatorTestCase {
 		assertEquals("Result: a + b = 5.0", calc.execute());
 		//REFACTORING: notice that fact of testing force us to think about verbs (methods) of our DSL
 	}
+	
+	//REFACTORING: more tests
+	@Test
+	public void testPlusWithNegatives(){
+		MainCalc calc = new MainCalc();
+		calc.inputFirstOperand(3.0);
+		calc.inputSecondOperand(-2.0);
+		calc.inputOperation("+");
+		assertEquals("Result: a + b = 1.0", calc.execute());
+	}
+	@Test
+	public void testMinus(){
+		MainCalc calc = new MainCalc();
+		calc.inputFirstOperand(77.0);
+		calc.inputSecondOperand(22);
+		calc.inputOperation("-");
+		assertEquals("Result: a - b = 55.0", calc.execute());
+	}
+	@Test
+	public void testMultiply(){
+		MainCalc calc = new MainCalc();
+		calc.inputFirstOperand(-4);
+		calc.inputSecondOperand(3.0);
+		calc.inputOperation("*");
+		assertEquals("Result: a * b = -12.0", calc.execute());
+	}
+	@Test
+	public void testDivision(){
+		MainCalc calc = new MainCalc();
+		calc.inputFirstOperand(100.0);
+		calc.inputSecondOperand(25.0);
+		calc.inputOperation("/");
+		assertEquals("Result: a / b = 4.0", calc.execute());
+	}
+	@Test
+	public void testDivisionByZero(){
+		MainCalc calc = new MainCalc();
+		calc.inputFirstOperand(100.0);
+		calc.inputSecondOperand(0);
+		calc.inputOperation("/");
+		assertEquals("Result: a / b = Infinity", calc.execute());
+	}
 
 }
