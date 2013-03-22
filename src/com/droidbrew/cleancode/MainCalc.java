b1dbb0e1bafad6a004268c75f2dedb7238497919
Aleksey Drobnych <adrobnych@gmail.com>
2013-03-22 20:28:20 +0200

    refactored method main

--- src/com/droidbrew/cleancode/MainCalc.java
+++ src/com/droidbrew/cleancode/MainCalc.java
@@ -5,30 +5,33 @@ import java.util.Scanner;
 public class MainCalc {
 	
 	public static void main(String[] args) {
-		
+		Scanner scan = new Scanner(System.in);
+		Calculator calculator = new Calculator();
+		calculator.inputFirstOperand(getFirstOperand(scan));
+		calculator.inputSecondOperand(getSecondOperand(scan));
+		calculator.inputOperation(getOperation(scan));
+		System.out.println(calculator.execute());
+		scan.close();
+	}
+
+	private static double getFirstOperand(Scanner scan){
 		System.out.println("Enter first operand: ");
 		double firstOperand;
-		Scanner scan = new Scanner(System.in);
 		firstOperand = scan.nextDouble();
 		scan.nextLine();
-		
+		return firstOperand;
+	}
+	
+	private static double getSecondOperand(Scanner scan){
 		System.out.println("Enter secondOperand: ");
 		double secondOperand;
 		secondOperand = scan.nextDouble();
 		scan.nextLine();
-		
-		System.out.println("Enter operation sign: ");
-		String operation = scan.nextLine();
-		
-		Calculator calculator = new Calculator();
-		calculator.inputFirstOperand(firstOperand);
-		calculator.inputSecondOperand(secondOperand);
-		calculator.inputOperation(operation);
-		
-		System.out.println(calculator.execute());
-		
-		scan.close();
-		
+		return secondOperand;
 	}
-
+	
+	private static String getOperation(Scanner scan){
+		System.out.println("Enter operation sign: ");
+		return scan.nextLine();
+	}
 }
